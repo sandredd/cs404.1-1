e96e44f6aa3ee7c3d1bb8d63492d1e5abb8eef2e
elser <62680156+mattelser@users.noreply.github.com>
2020-05-09 15:23:09 -0700

    cycle: add a test that Cycle class's cycle method matches actual cycle (#28)
    Co-authored-by: matt elser <elser@fullpuff.attlocal.net>

--- java/test/edu/berkeley/cs/graph/Cycle_T.java
+++ java/test/edu/berkeley/cs/graph/Cycle_T.java
@@ -1,5 +1,6 @@
 package edu.berkeley.cs.graph;
 
+import edu.berkeley.cs.util.LinkedList;
 import edu.berkeley.cs.util.Utilities;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +13,21 @@ public class Cycle_T {
     graph = Utilities.smallCyclicGraph();
     Cycle finder = new Cycle(graph);
     Assert.assertTrue(finder.hasCycle());
+  }
+
+  @Test
+  public void testCycleFound() {
+    graph = Utilities.smallCyclicGraph();
+    Cycle finder = new Cycle(graph);
+    LinkedList<Integer> expectedCycle = new LinkedList<>();
+    expectedCycle.insertFront(2);
+    expectedCycle.insertFront(3);
+    int actualCycleSize = 0;
+    for (int i : finder.cycle()){
+      actualCycleSize++;
+      Assert.assertTrue(expectedCycle.contains(i));
+    }
+    Assert.assertTrue(actualCycleSize == expectedCycle.size());
   }
 
   @Test
