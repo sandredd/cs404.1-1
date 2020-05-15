4894c3c098e2e87c2273fe81ee70312ac9c40e4c
Faraaz Sareshwala <faraazs@gmail.com>
2020-05-14 23:23:21 -0700

    java: remove comments from linked list -- causing merge problems for students -- will add it back later

--- java/src/edu/berkeley/cs/util/SplayList.java
+++ java/src/edu/berkeley/cs/util/SplayList.java
@@ -1,13 +1,6 @@
 package edu.berkeley.cs.util;
 
 public class SplayList<T> extends LinkedList<T> {
-  /**
-   * Check whether this list contains a specific value. If the value is found within the list, the
-   * value is moved to the front of the list so that future accesses will be faster.
-   *
-   * @param value The element whose presence in this list is to be tested
-   * @return true if this list contains the specified element
-   */
   @Override
   public boolean contains(T value) {
     return false;
--- java/src/edu/berkeley/cs/util/Stack.java
+++ java/src/edu/berkeley/cs/util/Stack.java
@@ -13,47 +13,22 @@ public class Stack<T> implements Iterable<T> {
     this.list = list;
   }
 
-  /**
-   * Get the value of the top element in the stack.
-   *
-   * @return the value of the top element in the stack.
-   */
   public T peek() {
     return list.front();
   }
 
-  /**
-   * Push a value onto the top of the stack.
-   *
-   * @param value the value to be added onto the top of the stack.
-   */
   public void push(T value) {
     list.insertFront(value);
   }
 
-  /**
-   * Remove a value from the top of the stack.
-   *
-   * @return the value that was previously on the top of the stack.
-   */
   public T pop() {
     return list.removeFront();
   }
 
-  /**
-   * Returns the number of elements in the stack.
-   *
-   * @return the number of elements in the stack.
-   */
   public int size() {
     return list.size();
   }
 
-  /**
-   * Returns true if this stack contains no elements.
-   *
-   * @return true if this stack contains no elements.
-   */
   public boolean isEmpty() {
     return list.isEmpty();
   }
