b25d31cf8b90064cb8c62aefce2141afef3a0e26
Faraaz Sareshwala <faraazs@gmail.com>
2020-05-14 23:18:59 -0700

    final: add tests for hot key generator

--- java/test/edu/berkeley/cs/app/HotKeyGenerator_T.java
+++ java/test/edu/berkeley/cs/app/HotKeyGenerator_T.java
@@ -1,3 +1,81 @@
 package edu.berkeley.cs.app;
 
-public class HotKeyGenerator_T {}
+import edu.berkeley.cs.util.HashMap;
+import edu.berkeley.cs.util.HashSet;
+import edu.berkeley.cs.util.LinkedList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HotKeyGenerator_T {
+  private HotKeyGenerator hotKeyGenerator;
+
+  @Before
+  public void setup() {
+    hotKeyGenerator = new HotKeyGenerator();
+  }
+
+  @Test
+  public void testEmptyActions() {
+    LinkedList<String> actions = new LinkedList<>();
+    HashMap<Character, String> result = hotKeyGenerator.generateHotKeys(actions);
+    Assert.assertTrue(result.isEmpty());
+  }
+
+  @Test
+  public void testSingleAction() {
+    LinkedList<String> actions = new LinkedList<>();
+    actions.insertFront("down");
+
+    HashMap<Character, String> result = hotKeyGenerator.generateHotKeys(actions);
+    Assert.assertTrue(result.contains('d'));
+    Assert.assertEquals("down", result.get('d'));
+  }
+
+  @Test
+  public void testMultipleActionsNoConflict() {
+    LinkedList<String> actions = new LinkedList<>();
+    actions.insertFront("up");
+    actions.insertFront("down");
+
+    HashMap<Character, String> result = hotKeyGenerator.generateHotKeys(actions);
+    Assert.assertEquals("up", result.get('u'));
+    Assert.assertEquals("down", result.get('d'));
+  }
+
+  @Test
+  public void testConflict() {
+    LinkedList<String> actions = new LinkedList<>();
+    actions.insertFront("up");
+    actions.insertFront("up");
+
+    HashMap<Character, String> result = hotKeyGenerator.generateHotKeys(actions);
+    Assert.assertEquals("up", result.get('p'));
+    Assert.assertEquals("up", result.get('u'));
+  }
+
+  @Test
+  public void testNoSolution() {
+    LinkedList<String> actions = new LinkedList<>();
+    actions.insertFront("a");
+    actions.insertFront("a");
+
+    HashMap<Character, String> result = hotKeyGenerator.generateHotKeys(actions);
+    Assert.assertTrue(result.isEmpty());
+  }
+
+  @Test
+  public void testExhaustive() {
+    LinkedList<String> actions = new LinkedList<>();
+    actions.insertFront("forward");
+    actions.insertFront("backward");
+    actions.insertFront("left");
+    actions.insertFront("right");
+    actions.insertFront("fly");
+    actions.insertFront("flyr");
+
+    int size = actions.size();
+    HashMap<Character, String> result = hotKeyGenerator.generateHotKeys(actions);
+    Assert.assertEquals(size, result.size());
+  }
+}
