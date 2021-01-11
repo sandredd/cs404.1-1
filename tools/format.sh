0479b066bf27ed469cf845ccce1837cb9e93bcb4
Faraaz Sareshwala <faraazs@gmail.com>
2021-01-10 20:51:06 -0800

    tools: format

--- tools/format.sh
+++ tools/format.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 function main() {
-    # format java
-    gitroot=$(git rev-parse --show-toplevel)
-    java_formatter=$gitroot/thirdparty/format/java/google-java-format-1.7-all-deps.jar
-    find $gitroot/java -name '*.java' | xargs java -jar $java_formatter --replace
+  # format java
+  gitroot=$(git rev-parse --show-toplevel)
+  java_formatter=$gitroot/thirdparty/format/java/google-java-format-1.7-all-deps.jar
+  find $gitroot/java -name '*.java' | xargs java -jar $java_formatter --replace
 }
 
 main "$@"
--- tools/nostdlib.sh
+++ tools/nostdlib.sh
@@ -1,48 +1,47 @@
 #!/usr/bin/env bash
 
 function join() {
-    local IFS="$1"
-    shift
-    echo "$*"
+  local IFS="$1"
+  shift
+  echo "$*"
 }
 
 function main() {
-    java_blessed=(
-        "java.io.BufferedReader"
-        "java.io.FileReader"
-        "java.io.IOException"
-        "java.lang.reflect.Array"
-        "java.nio.charset.CharsetEncoder"
-        "java.nio.charset.StandardCharsets"
-        "java.nio.file.Files"
-        "java.nio.file.Path"
-        "java.nio.file.Paths"
-        "java.util.Arrays"
-        "java.util.Iterator"
-        "java.util.NoSuchElementException"
-        "java.util.Objects"
-        "java.util.Random"
-        "java.util.concurrent.Callable"
-        "java.util.concurrent.ExecutionException"
-        "java.util.concurrent.ExecutorService"
-        "java.util.concurrent.Future"
-        "java.util.concurrent.LinkedBlockingQueue"
-        "java.util.concurrent.ThreadPoolExecutor"
-        "java.util.concurrent.TimeUnit"
-        "java.util.concurrent.TimeoutException"
-    )
+  java_blessed=(
+    "java.io.BufferedReader"
+    "java.io.FileReader"
+    "java.io.IOException"
+    "java.lang.reflect.Array"
+    "java.nio.charset.CharsetEncoder"
+    "java.nio.charset.StandardCharsets"
+    "java.nio.file.Files"
+    "java.nio.file.Path"
+    "java.nio.file.Paths"
+    "java.util.Arrays"
+    "java.util.Iterator"
+    "java.util.NoSuchElementException"
+    "java.util.Objects"
+    "java.util.Random"
+    "java.util.concurrent.Callable"
+    "java.util.concurrent.ExecutionException"
+    "java.util.concurrent.ExecutorService"
+    "java.util.concurrent.Future"
+    "java.util.concurrent.LinkedBlockingQueue"
+    "java.util.concurrent.ThreadPoolExecutor"
+    "java.util.concurrent.TimeUnit"
+    "java.util.concurrent.TimeoutException"
+  )
 
+  error_message="The following imports are invalid (you may not use the standard library):"
+  gitroot=$(git rev-parse --show-toplevel)
+  cd $gitroot
 
-    error_message="The following imports are invalid (you may not use the standard library):"
-    gitroot=$(git rev-parse --show-toplevel)
-    cd $gitroot
-
-    java_invalid="$(git grep 'import.*java.*' java | grep -Ev $(join '|' ${java_blessed[@]}))"
-    if [[ $java_invalid != "" ]]; then
-        echo "$error_message"
-        echo "$java_invalid"
-        exit 1
-    fi
+  java_invalid="$(git grep 'import.*java.*' java | grep -Ev $(join '|' ${java_blessed[@]}))"
+  if [[ $java_invalid != "" ]]; then
+    echo "$error_message"
+    echo "$java_invalid"
+    exit 1
+  fi
 }
 
 main "$@"
